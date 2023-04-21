de2bf4c857552f9767874fed19cfce51199b15ee
Daniel Tello Castillo <daniel@Daniel-MacBook.local>
2023-04-21 12:08:13 -0500

    first commit

---
+++ app/src/main/java/pe/edu/grupo3_asignacion1/ui/asignacion1/perfilModules/PerfilGrid.kt
@@ -0,0 +1,16 @@
+package pe.edu.grupo3_asignacion1.ui.asignacion1.perfilModules
+
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Preview
+@Composable
+fun PerfilGridPreview(){
+
+}
+
+@Composable
+fun PerfilGrid(){
+
+}
---
+++ app/src/main/java/pe/edu/grupo3_asignacion1/ui/asignacion1/uis/PerfilScreen.kt
@@ -0,0 +1,20 @@
+package pe.edu.grupo3_asignacion1.ui.asignacion1.uis
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import pe.edu.grupo3_asignacion1.ui.asignacion1.perfilModules.PerfilGrid
+
+@Preview
+@Composable
+public fun PerfilScreenPreview(){
+
+}
+@Composable
+public fun PerfilScreen(){
+    //Archivo Brillitt
+
+    //Archivo Gonzalo
+
+    //Archivo Daniel
+    PerfilGrid()
+}
