64f5282d03d2619faea32969a7b2ece2faab024d
Daniel Tello Castillo <daniel@Daniel-MacBook.local>
2023-04-22 01:07:59 -0500

    avance grilla

---
+++ app/src/main/java/pe/edu/grupo3_asignacion1/models/Data.kt
@@ -0,0 +1,8 @@
+package pe.edu.grupo3_asignacion1.models
+
+val PhotoList = listOf<Photo>(
+    Photo("https://images.pexels.com/photos/16407235/pexels-photo-16407235.jpeg","image1"),
+    Photo("https://images.pexels.com/photos/15561915/pexels-photo-15561915.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2","image2"),
+    Photo("https://images.pexels.com/photos/14245756/pexels-photo-14245756.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2","image3"),
+    Photo("https://images.pexels.com/photos/16041156/pexels-photo-16041156.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2","image4")
+)
