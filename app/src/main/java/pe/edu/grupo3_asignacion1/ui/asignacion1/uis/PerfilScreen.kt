dc11bbdae903fbe73df1a288849d2bd13f146ac4
ggarma <gonzalo.garma@sportscam.pe>
2023-04-23 12:29:47 -0500

    Parte Gonzalo

--- app/src/main/java/pe/edu/grupo3_asignacion1/ui/asignacion1/uis/PerfilScreen.kt
+++ app/src/main/java/pe/edu/grupo3_asignacion1/ui/asignacion1/uis/PerfilScreen.kt
@@ -2,6 +2,7 @@ package pe.edu.grupo3_asignacion1.ui.asignacion1.uis
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import pe.edu.grupo3_asignacion1.ui.asignacion1.perfilModules.HighlightsStories
 import pe.edu.grupo3_asignacion1.ui.asignacion1.perfilModules.PerfilGrid
 
 @Preview
@@ -14,6 +15,7 @@ public fun PerfilScreen(){
     //Archivo Brillitt
 
     //Archivo Gonzalo
+    HighlightsStories()
 
     //Archivo Daniel
     PerfilGrid()
